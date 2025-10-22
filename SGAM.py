d8f0569fc47ac99d7f729dc1fd6c2f07094374ad
Chaoyv <136290776+ZChaoyv@users.noreply.github.com>
2025-10-22 14:07:14 +0800

    Create SGAM.py

---
+++ SGAM.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class SGAM(nn.Module):
+    """
+    """
+    def __init__(self, in_channels, out_channels):
+        super(SGAM, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        
+        # 
+        # 
+        # 
+        # 
+        # 
+        # 
+        # 
+        # 
+
+    def forward(self, x1, x2):
+        
+        # 
+        # 
+        # 
+        # 
+        # 
+
+        return x
